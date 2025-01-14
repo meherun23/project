f5ef345b461dc9496393af6583f4f876e21de7b1
oroni-43 <mashiyathoroni@gmail.com>
2025-01-14 19:44:22 +0600

    From admin panel (Oroni)

---
+++ settings_crud.php
@@ -0,0 +1,47 @@
+<?php
+
+require('../inc/db_config.php');
+require('../inc/essentials.php');
+adminLogin();
+
+if (isset($_POST['get_general'])) {
+    $q = "SELECT * FROM `settings` WHERE `sr_no`=?";
+    $values = [1];
+    $res = select($q, $values, "i");
+    $data = mysqli_fetch_assoc($res);
+    $json_data = json_encode($data);
+    echo $json_data;
+}
+
+if (isset($_POST['upd_general']))
+ {
+    $frm_data = filteration($_POST);
+    $q = "UPDATE `settings` SET `site_title`=?, `site_about`=? WHERE `sr_no`=?";
+    $values = [$frm_data['site_title'],$frm_data['site_about'],1];
+    $res = update($q,$values,'ssi');
+    echo $res;
+ }
+
+ 
+if (isset($_POST['get_contacts'])) {
+    $q = "SELECT * FROM `contact_details` WHERE `sr_no`=?";
+    $values = [1];
+    $res = select($q, $values, "i");
+    $data = mysqli_fetch_assoc($res);
+    $json_data = json_encode($data);
+    echo $json_data;
+}
+
+if (isset($_POST['upd_contacts']))
+ {
+    $frm_data = filteration($_POST);
+    $q = "UPDATE `contact_details` SET `address`=?,`gmap`=?,`pn1`=?,`pn2`=?,`email`=?,`fb`=?,`insta`=?,`tw`=? WHERE `sr_no`=?";
+    $values = [$frm_data['address'],$frm_data['gmap'],$frm_data['pn1'],$frm_data['pn2'],$frm_data['email'],$frm_data['fb'],$frm_data['insta'],$frm_data['tw'],1];
+    $res = update($q,$values,'ssssssssi');
+    echo $res;
+ }
+
+
+
+
+?>
